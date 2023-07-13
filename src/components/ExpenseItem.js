9dc0d4c5b337fa95409737243e28b47ba4d987e4
Pandeyviii <vishalkumarpandey665@gmail.com>
2023-07-13 11:05:03 +0530

    parsing data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-function ExpenseItem(){
-    return (
-        <div>
-    <ul>ExpenseItems</ul>
-    <li>Food rs 100</li>
-    <li>petrol rs 500</li>
-    <li>movie rs 250</li>
+import './ExpenseItem.css'
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>
+        {props.date.toISOString()}
+      </div>
+      <div className="expense-item__description ">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price ">${props.amount}</div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default ExpenseItem;
