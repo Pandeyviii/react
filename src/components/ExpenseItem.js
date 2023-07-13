fa70af988416b16d534eca117a323f2a6ec96649
Pandeyviii <vishalkumarpandey665@gmail.com>
2023-07-13 09:53:28 +0530

    expense item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+        <div>
+    <ul>ExpenseItems</ul>
+    <li>Food rs 100</li>
+    <li>petrol rs 500</li>
+    <li>movie rs 250</li>
+    </div>
+    );
+}
+
+export default ExpenseItem;
