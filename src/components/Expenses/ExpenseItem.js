daebfa8f0ba1cae6dbdc66da7a636c6e3d6bfbc4
Pandeyviii <vishalkumarpandey665@gmail.com>
2023-07-14 09:58:35 +0530

    creating own first componenet

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
+import React,{useState} from 'react'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 const ExpenseItem=(props)=> {
+    const [title,setTitle]=useState(props.title);
     const clickHandler=()=>{
-        alert("button is clicked");
+        setTitle("updated!");
+        alert("title updated");
     }
   return (
     <div className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description ">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price ">${props.amount}</div>
       </div>
-      <button type='submit' onClick={clickHandler}>Delete Expenses</button>
+      <button type='submit' onClick={clickHandler}>change title</button>
     </div>
   );
 }
